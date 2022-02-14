8bc59e0a1b2ce593b28293d31315e64289655af6
Tuan Vu <vuanhtuanvn85@gmail.com>
2022-02-14 21:16:30 +0700

    init PoeAllInOne

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,5 @@
 import React, { createRef } from 'react'
-import {
-  Container,
-  Dimmer,
-  Loader,
-  Grid,
-  Sticky,
-  Message,
-} from 'semantic-ui-react'
+import { Container, Dimmer, Loader, Grid, Sticky, Message, } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 import { SubstrateContextProvider, useSubstrateState } from './substrate-lib'
@@ -19,6 +12,7 @@ import Events from './Events'
 import Interactor from './Interactor'
 import Metadata from './Metadata'
 import NodeInfo from './NodeInfo'
+import PoeType from './PoeType';
 import PoeAllInOne from './PoeAllInOne';
 import Transfer from './Transfer'
 import Upgrade from './Upgrade'
@@ -82,6 +76,7 @@ function Main() {
             <Events />
           </Grid.Row>
           <Grid.Row>
+            <PoeType />
             <PoeAllInOne />
           </Grid.Row>
         </Grid>
--- front-end/src/PoeAllInOne.js
+++ front-end/src/PoeAllInOne.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { Form, Input, Grid, Card, Statistic, Message } from 'semantic-ui-react'
+import { Form, Input, Grid, Card, Statistic, Message, Dropdown } from 'semantic-ui-react'
 
 import { useSubstrateState } from './substrate-lib'
 import { TxButton } from './substrate-lib/components'
 
 // Polkadot-JS utilities for hashing data.
 import { blake2AsHex } from '@polkadot/util-crypto';
+import { hexToString, u8aToString } from '@polkadot/util';
 
 function Main(props) {
   const { api } = useSubstrateState()
@@ -26,24 +27,67 @@ function Main(props) {
   const [field4Value, setField4Value] = useState('')
   const [field5Value, setField5Value] = useState('')
 
+  const [poeTypeNames, setPoeTypeNames] = useState([]);
+
+  const initFormState = {
+    poeTypeName: '',
+    callable: '',
+    inputParams: []
+  };
+
+  const [formState, setFormState] = useState(initFormState);
+  const { poeTypeName, callable, inputParams } = formState;
+
   const bufferToDigest = () => {
   const hash = blake2AsHex(nameValue, 256);
     setDigest(hash);
   };
 
-  useEffect(() => {
-    let unsubscribe
-    api.query.poeAllInOne
-      .poeTypes(digest, result => {
-        // Our storage item returns a tuple, which is represented as an array.
-        setOwner(result[7].toString());
-        setBlock(result[8].toNumber());
-      })
-      .then(unsub => {
-        unsubscribe = unsub;
-      });
-    return () => unsubscribe && unsubscribe();
-  }, [digest, api.query.poeAllInOne]);
+
+
+
+ 
+  const updatePoeTypeNames = () => {
+    if (!api) { return; }
+    const apiType = api.rpc;
+    console.log("apiType");
+    console.log(apiType);
+    console.log("poeTypes");
+    const temp = api.query.poeAllInOne.poeTypes.entries((result) => {
+      console.log("result");
+      console.log(result);
+      const poeTypeNames2 = result
+      .map(pr => ({ key: u8aToString(pr[0]), value: pr[0], text: pr[0] }));
+      console.log("poeTypeNames2");
+      console.log(poeTypeNames2);
+
+      setPoeTypeNames(poeTypeNames2);
+    });
+    console.log("Object.keys(apiType)");
+    console.log(Object.keys(apiType));
+    const poeTypeNames = Object.keys(apiType).sort()
+      .filter(pr => Object.keys(apiType[pr]).length > 0)
+      .map(pr => ({ key: pr, value: pr, text: pr }));
+    console.log("poeTypeNames");
+    console.log(poeTypeNames);
+
+  };
+
+  useEffect(updatePoeTypeNames, [api]);
+
+  // useEffect(() => {
+  //   let unsubscribe
+  //   api.query.poeAllInOne
+  //     .poeTypes(digest, result => {
+  //       // Our storage item returns a tuple, which is represented as an array.
+  //       setOwner(result[7].toString());
+  //       setBlock(result[8].toNumber());
+  //     })
+  //     .then(unsub => {
+  //       unsubscribe = unsub;
+  //     });
+  //   return () => unsubscribe && unsubscribe();
+  // }, [digest, api.query.poeAllInOne]);
 
 
   // We can say a file digest is claimed if the stored block number is not 0
@@ -51,87 +95,46 @@ function Main(props) {
     return block !== 0;
   }
 
+
+  const onPoeTypeNameChange = (_, data) => {
+    console.log(data.value);
+    console.log('onPoeTypeNameChange');
+    // setFormState(formState => {
+    //   let res;
+    //   const { state, value } = data;
+    //   if (typeof state === 'object') {
+    //     // Input parameter updated
+    //     const { ind, paramField: { type } } = state;
+    //     const inputParams = [...formState.inputParams];
+    //     inputParams[ind] = { type, value };
+    //     res = { ...formState, inputParams };
+    //   } else if (state === 'poeTypeName') {
+    //     res = { ...formState, [state]: value, callable: '', inputParams: [] };
+    //   } else if (state === 'callable') {
+    //     res = { ...formState, [state]: value, inputParams: [] };
+    //   }
+    //   return res;
+    // });
+  };
+
   return (
     <Grid.Column width={8}>
-      <h1>Create Poe Type</h1>
+      <h1>Create Proofs</h1>
       <Form success={!!digest && !isCreated()} warning={isCreated()}>
         <Form.Field>
-          <Input
-            label="Name "
-            state="name"
-            type="text"
-            onChange={(_, { value }) => {setNameValue(value); bufferToDigest();}}
-          />
-          {/* Show this message if the poe type is available to be created */}
-          <Message success header="Poe type name is available" />
-          {/* Show this message if the poe type is already created. */}
-          <Message
-            warning
-            header="Poe type name is taken"
-            list={[`Owner: ${owner}`, `Block: ${block}`]}
+          <Dropdown
+            placeholder='Poe Type Name'
+            fluid
+            label='Poe Type Name'
+            onChange={onPoeTypeNameChange}
+            search
+            selection
+            state='poeTypeName'
+            value={poeTypeName}
+            options={poeTypeNames}
           />
         </Form.Field>
-        <Form.Field>
-          <Input
-            label="Description"
-            state="description"
-            type="text"
-            onChange={(_, { value }) => {setDescriptionValue(value); bufferToDigest();}}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            label="Field 1   "
-            state="field_1"
-            type="text"
-            onChange={(_, { value }) => {setField1Value(value); bufferToDigest();}}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            label="Field 2"
-            state="field2_"
-            type="text"
-            onChange={(_, { value }) => {setField2Value(value); bufferToDigest();}}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            label="Field 3"
-            state="field_3"
-            type="text"
-            onChange={(_, { value }) => {setField3Value(value); bufferToDigest();}}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            label="Field 4"
-            state="field_4"
-            type="text"
-            onChange={(_, { value }) => {setField4Value(value); bufferToDigest();}}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            label="Field 5"
-            state="field_5"
-            type="text"
-            onChange={(_, { value }) => {setField5Value(value); bufferToDigest();}}
-          />
-        </Form.Field>
-        <Form.Field style={{ textAlign: 'center' }}>
-          <TxButton
-            label="Create Poe Type"
-            type="SIGNED-TX"
-            setStatus={setStatus}
-            attrs={{
-              palletRpc: 'poeAllInOne',
-              callable: 'createPoeType',
-              inputParams: [digest, nameValue, descriptionValue, field1Value, field2Value, field3Value, field4Value, field5Value],
-              paramFields: [true, true, true, true, true, true, true, true],
-            }}
-          />
-        </Form.Field>
+        
         <div style={{ overflowWrap: 'break-word' }}>{status}</div>
       </Form>
     </Grid.Column>
